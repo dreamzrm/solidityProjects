6a26fc4ad5cd5e1eb56c3d61c0db64b349480cc3
varan <82107909+dreamzrm@users.noreply.github.com>
2023-01-13 18:22:43 +0530

    Update 777lottery.sol

--- 777lottery.sol
+++ 777lottery.sol
@@ -1,33 +1,55 @@
 pragma solidity ^0.8.11;
 
 contract lottery {
+    //mapping(address => uint) public lotteryOwners;
     mapping(address => uint) public lotteryOwners;
-    uint sold;
+    uint public ticketsLeft;
+    uint totalTickets;
     uint lotteryPrice = 5;
     address owner;
+    address payable public winner;
 
     modifier cost() {
         require(msg.value >= lotteryPrice, "Not enough money");
         _;
     }
 
-    modifier ticketsLeft {
-        require(sold > 0, "The lottery is sold out.");
+    modifier ticketsReallyLeft {
+        require(ticketsLeft > 0, "The lottery is sold out.");
         _;
     }
 
-    constructor(uint _sold){
+    modifier ownerOnly {
+        require(msg.sender == owner, "The owner get to help in picking the owner");
+        _;
+    }
+
+    modifier noTicketsLeft {
+        require( ticketsLeft < 1, "Tickets still left");
+        _;
+    }
+
+    constructor(uint _ticketsLeft){
         owner = msg.sender;
-        sold = _sold;
+        ticketsLeft = _ticketsLeft;
+        totalTickets = _ticketsLeft;
+
     }
 
     function createTicket() internal returns (uint) {
-        uint ticket = 900000 + sold;
-        sold--;
+        uint256 ticket = 900000 + ticketsLeft;
+        ticketsLeft--;
         return ticket;
     }
 
-    function getTicket() external payable ticketsLeft cost() {
+    function getTicket() external payable ticketsReallyLeft cost() {
         lotteryOwners[msg.sender] = createTicket();
     }
+
+    function pickWinner() external payable ownerOnly noTicketsLeft {
+        //Randomly pick a winner and put all the ether in their account
+        winner = payable(0x583031D1113aD414F02576BD6afaBfb302140225);
+        winner.transfer(address(this).balance);
+    }
+
 }
