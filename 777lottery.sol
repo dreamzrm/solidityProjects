353bd8f3270dd5e9f0cb6ecb2a3362122d5f3c41
varan <82107909+dreamzrm@users.noreply.github.com>
2023-01-14 18:20:31 +0530

    Update 777lottery.sol

--- 777lottery.sol
+++ 777lottery.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.8.11;
+pragma solidity 0.8.7;
 
 contract lottery {
     //mapping(address => uint) public lotteryOwners;
     mapping(address => uint) public lotteryOwners;
+    mapping(uint => address) checker;
     uint public ticketsLeft;
     uint totalTickets;
     uint lotteryPrice = 5;
@@ -38,17 +39,24 @@ contract lottery {
 
     function createTicket() internal returns (uint) {
         uint256 ticket = 900000 + ticketsLeft;
-        ticketsLeft--;
         return ticket;
     }
 
     function getTicket() external payable ticketsReallyLeft cost() {
         lotteryOwners[msg.sender] = createTicket();
+        checker[ticketsLeft] = msg.sender;
+        ticketsLeft--;
+    }
+
+    function random() private view returns (uint) {
+        uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, ticketsLeft)));
+        return randomHash % totalTickets;
     }
 
     function pickWinner() external payable ownerOnly noTicketsLeft {
         //Randomly pick a winner and put all the ether in their account
-        winner = payable(0x583031D1113aD414F02576BD6afaBfb302140225);
+        uint player = random();
+        winner = payable(checker[player]);
         winner.transfer(address(this).balance);
     }
 
